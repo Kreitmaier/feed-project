87a19c9797c9a44be81734204c33986a7dfbeeda
Douglas K Lopes <douglasklopes@hotmail.com>
2022-09-09 17:57:42 -0300

    created Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { Header } from "./components/Header"
+
+import "./global.css";
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Header />
+      <h1>Hello World!</h1>
+    </>
+    
   )
 }
 
