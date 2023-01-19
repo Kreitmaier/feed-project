8d94d752e8f6fbbc09f9d9186d3b6fcb5311831a
Douglas Kreitmaier <douglasklopes@gmail.com>
2023-01-19 07:25:56 -0300

    adds responsive funcionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
-
-
 import styles from './App.module.css';
 
 import "./global.css";
